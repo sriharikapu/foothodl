99553e8a9be056c4c48b5dfee21a5f8ee66274e0
James L <thelevybreaks@gmail.com>
2019-02-16 04:46:51 -0700

    restructured, added wallet and landing page

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# foothodl
-Foothodl - ETH Denver
+# FOOTHODL
+
+This project aims to provide a financial foothold to people in need, with the assistance of a network of volunteer ambassadors.
+
+The xDai Ethereum sidechain is used for fast transactions, and a modified version of Austin Griffith's [burner wallet](https://github.com/austintgriffith/burner-wallet) is used for redeeming donated funds. 
+
+## Local Server Development
+
+Use [virtualenv](https://virtualenv.pypa.io/en/stable/) to set up each
+service's environment and start the each service in a separate terminal.
+
+Open a terminal and start the first service:
+
+```Bash
+$ cd foothodl-server
+$ virtualenv -p python3 env
+$ source env/bin/activate
+$ pip3 install -r requirements.txt
+$ python3 main.py
+```
+
+The server should now be running at `http://localhost:8000`. 
+
+## Setting up GCloud Service Account 
+
+Follow the instructions at `https://cloud.google.com/docs/authentication/` and create file `gcloud.json` in the `foothodl-server` directory containing credentials JSON.
+
+## Setting up Twilio
+
+Copy `.env.example` to `.env` and modify the values to Twilio credentials. The `python-dotenv` library will load these values into the Flask application.
+
+## Deployment
+
+These instructions are specific to Google Cloud. 
+
+Make sure you have an App Engine project setup first, and have `gcloud` configured on your machine for that project.
+
+Run the following command to deploy:
+
+```Bash
+$ gcloud app deploy app.yaml
+```
+
+Enter `Y` when prompted.  Or to skip the check add `-q`.
+
+The deployed url will be `https://foothodl.<your project id>.appspot.com`
+
---
+++ main.py
@@ -0,0 +1,164 @@
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from flask import Flask, render_template
+import requests
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+from twilio.rest import Client as TwilioClient
+from google.cloud import datastore
+app = Flask(__name__, static_folder="static", template_folder="templates")
+
+def ambassador_key(client, number):
+    # [START datastore_named_key]
+    key = client.key('FoothodlAmbassador', number)
+    # [END datastore_named_key]
+    return key
+    
+def member_key(client, number):
+    # [START datastore_named_key]
+    key = client.key('FoothodlMember', number)
+    # [END datastore_named_key]
+    return key
+    
+def generate_confirmation_code():
+    import random
+    return random.randInt(100, 999)
+    
+def format_number(number):
+    if number[0] == '1':
+        return number[1:]
+    return number
+
+@app.route('/')
+def landing_page():
+    return render_template("index.html")
+
+def add_ambassador(ambassador_number):
+    ambassador_number = format_number(ambassador_number)
+    client = datastore.Client.from_service_account_json('gcloud.json')
+    key = ambassador_key(client, ambassador_number)
+    ambassador = client.get(key)
+    
+    if ambassador:
+        return f'Ambassador {ambassador_number} already exists'
+    else:
+        ambassador = datastore.Entity(key=key)
+        ambassador.update({
+            'number': ambassador_number,
+            'status': 'created'
+        })
+        client.put(ambassador)
+        
+        twilio_client = TwilioClient(
+            os.getenv('TWILIO_ACCOUNT_SID'), os.getenv('TWILIO_AUTH_TOKEN'))
+        wallet_url = f'http://www.foothodl.com/{ambassador_number}'
+        message = twilio_client.messages.create(to="+1%s" % ambassador_number, 
+        from_="+%s" % os.getenv('TWILIO_NUMBER'), 
+            body=f"You have been invited to FOOTHODL as an ambassador.  To redeem funds, provide a member's phone number in your wallet - {wallet_url}")
+        return 'Sent ambassador invite to %s' % ambassador_number
+    
+
+def add_member(member_number):
+    member_number = format_number(member_number)
+    client = datastore.Client.from_service_account_json('gcloud.json')
+    key = member_key(client, member_number)
+    member = client.get(key)
+    if member:
+        return f'Member {member_number} already exists'
+    else:
+        member = datastore.Entity(key=key)
+        member.update({
+            'number': member_number,
+            'status': 'created'
+        })
+        client.put(member)
+        
+        twilio_client = TwilioClient(
+            os.getenv('TWILIO_ACCOUNT_SID'), os.getenv('TWILIO_AUTH_TOKEN'))
+        message = twilio_client.messages.create(to=f"+1{member_number}", 
+        from_="+%s" % os.getenv('TWILIO_NUMBER'), 
+            body="You have been invited to FOOTHODL. Provide your phone number to a volunteer to get their assistance.")
+        return f'Sent member invite to {member_number}'
+        
+
+@app.route('/register/<ambassador_number>/<member_number>')
+def register(ambassador_number, member_number):
+    add_ambassador(ambassador_number)
+    add_member(member_number)
+    return 'OK'
+        
+@app.route('/request/<ambassador_number>/<member_number>/<amount>')
+def request_for_member(ambassador_number, member_number, amount):
+        member_number = format_number(member_number)
+        ambassador_number = format_number(ambassador_number)
+        client = datastore.Client.from_service_account_json('gcloud.json')
+        key = member_key(client, member_number)
+        member = client.get(key)
+        if not member:
+            raise Exception(f'member not found for number {member_number}')
+        member_number = member.number
+        code = generate_confirmation_code()
+        member.confirmation_code = code 
+        member.requested_amount = amount
+        client.put(member)
+        twilio_client = TwilioClient(
+            os.getenv('TWILIO_ACCOUNT_SID'), os.getenv('TWILIO_AUTH_TOKEN'))
+        message = twilio_client.messages.create(to=f"+1{member_number}", 
+        from_="+%s" % os.getenv('TWILIO_NUMBER'), 
+            body=f"Your FOOTHODL confirmation code for {amount} is: {code}")
+        return 'OK'
+        
+@app.route('/confirm/<ambassador_number>/<member_number>/<confirmation_code>')
+def confirm_request(ambassador_number, member_number, confirmation_code):
+        ambassador_number = format_number(ambassador_number)
+        client = datastore.Client.from_service_account_json('gcloud.json')
+        key = member_key(client, member_number)
+        member = client.get(key)
+        if not member:
+            raise Exception(f'member not found for number {member_number}')
+        if not member.confirmation_code or confirmation_code != member.confirmation_code:
+            raise Exception(f'confirmation code {confirmation_code} does not match saved code')
+        key = ambassador_key(client, ambassador_number)
+        ambassador = client.get(key)
+        member.requested_amount = None 
+        member.confirmation_code = None 
+        client.put(member)
+        return 'OK'
+        
+@app.route('/saveAddress/<ambassador_number>/<ambassador_address>')
+def saveAddress(ambassador_number, ambassador_address):
+    ambassador_number = format_number(ambassador_number)
+    client = datastore.Client.from_service_account_json('gcloud.json')
+    key = ambassador_key(client, ambassador_number)
+    ambassador = client.get(key)
+    if not ambassador:
+        raise Exception(f'ambassador {ambassador_number} not found')
+    if ambassador.address:
+        raise Exception(f'ambassador {ambassador_number} already has address saved.')
+    ambassador.address = ambassador_address
+    client.put(ambassador)
+    return f'Saved address {ambassador_address}'  
+    
+@app.route('/<int:number>')
+def wallet(number):
+    return render_template("wallet.html", number=number)
+
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8000, debug=True)
